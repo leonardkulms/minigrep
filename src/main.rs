75c0ffba66d7ebc60475f92f5887dd57076f4d42
Leonard Kulms <leonard.kulms@icloud.com>
2020-12-31 17:30:11 +0100

    outsource parsing the config to function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::env;
-use minigrep::run as run;
+use minigrep;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    run(&args);   
+    let (query, filename) = parse_config(&args);
+    minigrep::run(query, filename);   
+}
+
+fn parse_config(args: &Vec<String>) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
