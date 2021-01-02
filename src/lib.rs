c35fd17ab68c80d40fc93980b8f884a2384db8ce
Leonard Kulms <leonard.kulms@icloud.com>
2021-01-02 15:24:33 +0100

    write first basic test

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,18 @@ impl Config {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    println!("With text:\n{}", contents);
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
