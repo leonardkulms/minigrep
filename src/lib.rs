ce1906a82138e36491b51358a52dfa36d4a20353
Leonard Kulms <leonard.kulms@icloud.com>
2021-01-02 15:24:55 +0100

    write function that makes test pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
 use std::fs;
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result_vec: Vec<&str> = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result_vec.push(line);
+        }
+    }
+    result_vec
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
     for line in search(&config.query, &contents) {
