2759bc91f8054ee2148e177fef4d69c814d7d6cc
Leonard Kulms <leonard.kulms@icloud.com>
2021-01-02 16:11:22 +0100

    add failing test for insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -49,4 +49,19 @@ Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
