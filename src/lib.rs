8ff49e562f1fd444d2aebc998789ade4670ebcc8
Leonard Kulms <leonard.kulms@icloud.com>
2021-01-02 15:24:16 +0100

    move current code into library

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,29 @@
 use std::fs;
 
-pub fn run(query: &str, filename: &str) {
-    println!("searching for {:#?} in {:#?}", query, filename);
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+    Ok(())
+}
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
+pub struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
+}
+
 
     println!("With text:\n{}", contents);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use std::env;
-use minigrep;
+use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_config(&args);
-    minigrep::run(query, filename);   
-}
 
-fn parse_config(args: &Vec<String>) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    (query, filename)
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
