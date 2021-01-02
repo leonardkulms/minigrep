660db989ad77bff8cb0d5411a18a797d5a95e4ad
Leonard Kulms <leonard.kulms@icloud.com>
2021-01-02 16:12:15 +0100

    add case sensitive option in config

--- src/lib.rs
+++ src/lib.rs
@@ -20,8 +20,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub struct Config {
-    query: String,
-    filename: String,
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -31,7 +32,8 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
